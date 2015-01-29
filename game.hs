cfa5df082fabc08c5419d97e8d0ee109cddb5f36
nicolashahn <nhahn@ucsc.edu>
2015-01-29 14:12:02 -0800

    trying grid library

--- game.hs
+++ game.hs
@@ -8,14 +8,29 @@
 -- | Display "Hello World" in a window.
 --
 import Graphics.Gloss
+import GHC.Float
+import Math.Geometry.Grid
 
+cellSize :: Int
+cellSize = 5
+
+cellFloat :: Float
+cellFloat = int2Float cellSize
+
+gridWidth :: Int
+gridWidth = cellSize * 150
+
+gridHeight :: Int
+gridHeight = cellSize * 100.0
+
+board = rectSquareGrid 50 50
 
 
 main 	
  = display 
         (InWindow
 	       "Bacteria" 	 -- window title
-		(800, 600) 	 -- window size
+		(gridWidth, gridHeight) 	 -- window size
 		(400, 200)) 	 -- window position
 	black			 -- background color
 	picture			 -- picture to display
@@ -24,4 +39,4 @@ picture
 	= Translate (0) (0) -- shift the text to the middle of the window
 	$ Scale 1 1
 	$ Color white
-	$ Polygon (rectanglePath 5 5)
+	$ Polygon (rectanglePath cellFloat cellFloat)
