b1eb74d1e7fe1594fd21a73a57bee553db839c0e
nicolashahn <nhahn@ucsc.edu>
2015-01-30 15:11:54 -0800

    prints list of bacteria

--- game.hs
+++ game.hs
@@ -8,35 +8,52 @@
 -- | Display "Hello World" in a window.
 --
 import Graphics.Gloss
-import GHC.Float
+import Graphics.Gloss.Interface.IO.Game
 import Math.Geometry.Grid
+import Math.Geometry.Grid.Square
 
-cellSize :: Int
-cellSize = 5
-
-cellFloat :: Float
-cellFloat = int2Float cellSize
-
-gridWidth :: Int
-gridWidth = cellSize * 150
-
-gridHeight :: Int
-gridHeight = cellSize * 100.0
+winSize :: Int
+winSize = 500
 
 board = rectSquareGrid 50 50
 
+cellSize :: Float
+cellSize = 10
 
-main 	
- = display 
-        (InWindow
-	       "Bacteria" 	 -- window title
-		(gridWidth, gridHeight) 	 -- window size
-		(400, 200)) 	 -- window position
-	black			 -- background color
-	picture			 -- picture to display
+bacteria :: [(Float, Float)]
+bacteria = [(0,0),(1,0),(0,1),(25,25)]
 
-picture	
-	= Translate (0) (0) -- shift the text to the middle of the window
+-- Make coordinates correct for gloss display
+getX :: (Float, Float) -> Float
+getX (x,_) = (cellSize * x) - 245
+--getX (x,_) = (cellSize * x) - ((fromIntegral winSize) / 2) - div cellSize 2)
+
+getY :: (Float, Float) -> Float
+getY (_,y) = (cellSize * y) - 245
+--getY (_,y) = (cellSize * y) - ((fromIntegral winSize) / 2) - div cellSize 2)
+
+
+oneBacteria :: Float -> Float -> Picture
+oneBacteria x y
+	= Translate (x) (y)
 	$ Scale 1 1
 	$ Color white
-	$ Polygon (rectanglePath cellFloat cellFloat)
+	$ Polygon (rectanglePath cellSize cellSize)
+
+
+--take list of bacteria coordinates and make list of pictures out of them
+makeBacteria :: [(Float, Float)] -> [Picture]
+makeBacteria [] = []
+makeBacteria (c:cs) = (oneBacteria (getX c) (getY c)): makeBacteria cs
+
+allBacteria = Pictures (makeBacteria bacteria)
+
+
+main =
+	display
+        (InWindow
+	       "Bacteria" 	 -- window title
+		(winSize, winSize) 	 -- window size
+		(400, 200)) 	 -- window position
+		black			 -- background color
+		allBacteria			 -- picture to display
