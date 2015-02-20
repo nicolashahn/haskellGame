b6636c7ef336df7e46db802b479ecf23b2b580a1
nicolashahn <nhahn@ucsc.edu>
2015-02-20 12:48:54 -0800

    changed gameover screen, initial grid

--- main.hs
+++ main.hs
@@ -7,16 +7,17 @@ import Math.Geometry.Grid
 import Math.Geometry.Grid.Square
 import Data.List
 import System.Random
+import System.Random.Shuffle
 
 ------------------------------------------------------------------------------
 -- Initialization --
 ------------------------------------------------------------------------------
 
 gridLength :: Int
-gridLength = 25 -- length of grid
+gridLength = 40 -- length of grid
 
 cellSize :: Int
-cellSize = 25 -- cell's pixel height/width
+cellSize = 20 -- cell's pixel height/width
 cellFloat :: Float
 cellFloat = fromIntegral cellSize
 
@@ -27,22 +28,29 @@ winFloat = fromIntegral winSize
 
 grid = rectSquareGrid gridLength gridLength
 
-data Board = Play Player Enemy StdGen 
-          | GameOver
+data Board = Play Colony Colony StdGen 
+          | GameOver String
           deriving (Show)
 
 type Bacteria = Int
 type Position = (Int, Int)
-type Player = [Cell]
-type Enemy = [Cell]
+type Colony = [Cell]
 
 data Cell = Cell Bacteria Position Color
     deriving (Eq, Show)
 
-initialBoard :: StdGen -> Board 
+colorP = green :: Color
+colorE = red :: Color
+
+initPlayerPos :: Position
+initPlayerPos = (gridLength `div` 4, gridLength `div` 4)
+initEnemyPos :: Position
+initEnemyPos = (gridLength - (gridLength `div` 4), gridLength - (gridLength `div` 4))
+
+initialBoard :: StdGen -> Board
 initialBoard gen = Play
-            [Cell 1 (gridLength `div` 4, gridLength `div` 2) green] 
-            [Cell 1 (gridLength - (gridLength `div` 4), gridLength `div` 2) yellow]
+            [Cell 1 initPlayerPos green]
+            [Cell 1 initEnemyPos red]
             gen
 
 ------------------------------------------------------------------------------
@@ -54,11 +62,11 @@ drawColony cells = pictures [makeSquare x y col <> showNum n x y
                             | Cell n (x, y) col <- cells]
 
 drawBoard :: Board -> Picture
-drawBoard GameOver
-    = scale 0.5 0.5
-    $ translate (-400.0) (0.0)
+drawBoard (GameOver t)
+    = scale 0.3 0.3
+    $ translate (-winFloat/2 ) (0.0)
     $ color red
-    $ text  "GAME OVER"
+    $ text t
 
 drawBoard (Play cellsP cellsE gen) 
     = pictures [printGrid, drawColony cellsP, drawColony cellsE]
@@ -69,70 +77,92 @@ drawBoard (Play cellsP cellsE gen)
 -- Simulation --
 ------------------------------------------------------------------------------
 
--- probability that bacteria will grow
--- probGrowth = 1
+-- TODO: shuffle list before checking
+-- pick a position that each bacteria could spawn
+randPos :: [Position] -> Bacteria -> StdGen -> (Maybe Position, StdGen)
+randPos [] _ gen  = (Nothing, gen)
+randPos (p:ps) b gen 
+    | b > randNum = (Just p, newGen)
+    | otherwise = randPos ps b newGen
+    where (randNum, newGen) = randomR(1, 9) gen 
 
--- takes list of cell positions
--- returns list of adjacent positions
-adjCells :: [Position] -> [Position]
+-- possibly grow cells and update existing cells
+grow :: Colony -> [Maybe Position] -> Color -> Colony
+grow [] _ _ = []
+grow _ [] _ = []
+grow (c@(Cell pop xy colr):cs) (Just p:ps) colrNew
+    = (Cell 1 p colrNew) : (Cell (pop - (1)) xy colr) : grow cs ps colr
+grow (c:cs) (Nothing:ps) colrNew
+    = c : grow cs ps colrNew
+
+-- pick position to spawn at every index
+pickSpawns :: [[Position]] -> [Bacteria] -> StdGen -> [Maybe Position]
+pickSpawns [] _ _ = []
+pickSpawns (p:ps) (b:bs) gen 
+    = spawnPos : pickSpawns ps bs newGen
+    where
+        (spawnPos, newGen) = randPos (shuffle' p (length p) gen) b gen
+
+
+
+-- list of list of places bacteria could spawn. each list within a list corresponds to an
+-- index in the colony that the bacteria would spawn from 
+spawnPotential :: Colony -> Colony -> [[Position]]
+spawnPotential c1 c2 = map (\\ filledCells) adjC 
+    where filledCells = (colonyPos c1 ++ colonyPos c2)
+          adjC = adjCells (colonyPos c1)
+
+
+-- list of list of all neighbors of colony's bacteria 
+adjCells :: [Position] -> [[Position]]
 adjCells [] = []
-adjCells ps = nub (foldl (\a p -> a ++ (neighbours grid p)) [] ps)
-
--- returns positions of empty cells around border of clony
-borderCells :: [Position] -> [Position] -> [Position]
-borderCells [] __ = []
-borderCells ps filledCells = adjCells ps \\ filledCells
-
--- combines new cells
-combine :: [Position] -> Color -> [Cell]
-combine [] _ = []
-combine borderPos col = (Cell 1 (head borderPos) col) : combine (tail borderPos) col
+adjCells ps = map (neighbours grid) ps
 
 -- increases size of colony
-growColony :: [Cell] -> [Cell] -> [Cell]
-growColony c1 c2 = combine (borderPos c1 c2) (cellCol $ head c1) ++ c1
-    where borderPos c1 c2 = borderCells (colonyPos c1) (colonyPos c1 ++ colonyPos c2)
-
+growColony :: Colony -> Colony -> Color -> StdGen -> Colony 
+growColony c1 c2 colr gen 
+    = grow c1 chosenSpawns colr
+    where 
+        chosenSpawns = pickSpawns (spawnPotential c1 c2) popList gen
+        popList = map cellPop c1
+--
 -- updates population of one cell 
 upCellPop :: Cell -> Cell
 upCellPop c@(Cell pop xy col) = if pop < 10
-                              -- replace this 1 with a randZeroOne when it works
                               then (Cell (pop + (1)) xy col)
                               else c
 
 -- update list of bacteria's population
-updateCells :: [Cell] -> [Cell]
+updateCells :: Colony -> Colony
 updateCells [] = []
 updateCells cells = map upCellPop cells
 
 -- take a previous game state and return the new game state after given time
 simulateBoard :: Float -> (Board -> Board)
-simulateBoard _ GameOver = GameOver
-simulateBoard timeStep (Play cellsP cellsE gen)
-    | length cellsP >= 200 = GameOver
-    | otherwise = Play (fullUpdate cellsP cellsE) (fullUpdate cellsE cellsP) newGen
+simulateBoard _ (GameOver t) = (GameOver t)
+simulateBoard timeStep (Play colonyP colonyE gen)
+    | (length colonyP) + (length colonyE) >= (gridLength * gridLength) = GameOver (
+        if (length colonyP) > (length colonyE) 
+            then "Player Wins: " ++ (show (length colonyP)) ++ " cells"
+            else "Enemy Wins: " ++ (show (length colonyE)) ++ " cells"
+        )
+    | otherwise = Play 
+                  (fullUpdate colonyP colonyE colorP genP) 
+                  (fullUpdate colonyE colonyP colorE genE) 
+                  genNew
     where
-        (probGrowth, newGen) = randomR(1, 2) gen :: (Int, StdGen)
-        fullUpdate c1 c2 
-            | probGrowth == 1 = growColony (updateCells c1) c2
-            | otherwise = updateCells c1
-    -- = let   (probGrowth, newGen) = randomR(1, 2) gen :: (Int, StdGen)
-    --   in    Play (fullUpdate cellsP cellsE) (fullUpdate cellsE cellsP) newGen
-    --     where
-    --         fullUpdate c1 c2 
-    --             | probGrowth == 1 = growColony (updateCells c1) c2
-    --             | otherwise = updateCells c1
-    --
-
+        (genThis, genNew) = split gen
+        (genP, genE) = split genThis
+        fullUpdate c1 c2 colr g = growColony (updateCells c1) c2 colr g 
 
 ------------------------------------------------------------------------------
 -- Event handling --
 ------------------------------------------------------------------------------
 handleEvents :: Event -> Board -> Board
-handleEvents _ GameOver = GameOver
+handleEvents _ (GameOver t) = (GameOver t)
 handleEvents (EventKey (MouseButton LeftButton) Down _ _)
              (Play cellsP cellsE gen)
-    | length cellsP >= 20 = GameOver
+    | length cellsP >= 20 = GameOver "Game over"
     | otherwise = Play (Cell 1 (0, 0) blue : (concatMap updateCell cellsP)) (Cell 1 (0, 10) yellow : concatMap updateCell cellsE) gen
     where
         updateCell :: Cell -> [Cell]
@@ -164,7 +194,7 @@ gridSquares indices = map (\(x, y) -> makeSquare x y $ col x y) indices
 showNum :: Int -> Int -> Int -> Picture
 showNum i x y
     =  translate (getC(x) - cellFloat/5) (getC(y)-cellFloat/4)
-    $ scale (0.12) (0.12)
+    $ scale (0.1) (0.1)
     $ color black
     $ text (show i)
 
@@ -173,8 +203,8 @@ colonyPos :: [Cell] -> [Position]
 colonyPos cells = map cellPos cells
 
 -- get cell's color
-cellCol :: Cell -> Color
-cellCol (Cell _ _ col) = col
+cellColor :: Cell -> Color
+cellColor (Cell _ _ col) = col
 
  -- get cell's position
 cellPos :: Cell -> (Int, Int)
@@ -189,9 +219,9 @@ cellPop (Cell p _ _) = p
 ------------------------------------------------------------------------------
 main
  = do   gen <- getStdGen
-        play (InWindow "Grid" (winSize, winSize) (0, 0)) 	 -- window positioned in center
+        play (InWindow "Grid" (winSize, winSize) (0, 0))     -- window positioned in center
              white
-             1
+             10
              (initialBoard gen)
              drawBoard
              handleEvents
